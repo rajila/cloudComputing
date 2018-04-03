e45366d426d3369b11694753f201b3dfd5840fee
Ronald Daniel Ajila Calva <rajila@espol.edu.ec>
2018-04-03 19:38:00 +0200

    Proyecto de webAPI y Cliente

---
+++ practicaWebAPI/practicaWebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using practicaWebAPI.Models;
+
+namespace practicaWebAPI.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        private practicaWebAPIContext db = new practicaWebAPIContext();
+
+        // GET: api/Contacts
+        public IQueryable<Contacts> GetContacts()
+        {
+            return db.Contacts;
+        }
+
+        // GET: api/Contacts/5
+        [ResponseType(typeof(Contacts))]
+        public async Task<IHttpActionResult> GetContacts(int id)
+        {
+            Contacts contacts = await db.Contacts.FindAsync(id);
+            if (contacts == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contacts);
+        }
+
+        // PUT: api/Contacts/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutContacts(int id, Contacts contacts)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contacts.ContactId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contacts).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Contacts
+        [ResponseType(typeof(Contacts))]
+        public async Task<IHttpActionResult> PostContacts(Contacts contacts)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contacts.Add(contacts);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = contacts.ContactId }, contacts);
+        }
+
+        // DELETE: api/Contacts/5
+        [ResponseType(typeof(Contacts))]
+        public async Task<IHttpActionResult> DeleteContacts(int id)
+        {
+            Contacts contacts = await db.Contacts.FindAsync(id);
+            if (contacts == null)
+            {
+                return NotFound();
+            }
+
+            db.Contacts.Remove(contacts);
+            await db.SaveChangesAsync();
+
+            return Ok(contacts);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContactsExists(int id)
+        {
+            return db.Contacts.Count(e => e.ContactId == id) > 0;
+        }
+    }
+}
